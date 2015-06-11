7446f34fab0c77e219f61b7966b2a9214928de3c
junkka <johan.junkka@gmail.com>
2015-06-11 08:40:52 +0200

    init

--- render_pres.R
+++
@@ -1,7 +0,0 @@
-render_pres <- function(){
-  unlink("output/*", recursive = TRUE)
-  library(rmarkdown)
-  render("index.Rmd", output_dir="output")
-  render("dplyr.Rmd", output_dir="output")
-  file.copy('img', "output", recursive = TRUE)
-}
